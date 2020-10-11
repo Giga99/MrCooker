28ced01469db0c11a6eca7a5874232b1c34e2459
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2020-10-11 13:35:32 +0200

    Added Crashlytics and Firebase Performances support

--- app/src/main/java/mr/cooker/mrcooker/ui/activities/MainActivity.kt
+++ app/src/main/java/mr/cooker/mrcooker/ui/activities/MainActivity.kt
@@ -60,6 +60,9 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // TODO back button pressed findNavController().popBackStack()
+        // TODO splash screen
+
         checkNetworkConnectivity()
     }
 
