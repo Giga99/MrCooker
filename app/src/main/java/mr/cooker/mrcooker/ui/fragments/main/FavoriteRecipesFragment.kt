7577d2d94f4e7f43888b8e591f09c884eab202a5
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2020-10-18 23:56:16 +0200

    Added FavoriteRecipesFragment

---
+++ app/src/main/java/mr/cooker/mrcooker/ui/fragments/main/FavoriteRecipesFragment.kt
@@ -0,0 +1,7 @@
+package mr.cooker.mrcooker.ui.fragments.main
+
+import androidx.fragment.app.Fragment
+import mr.cooker.mrcooker.R
+
+class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
+}
